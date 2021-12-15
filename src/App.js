522aa85618edca3090f990793f121042995d92a3
Anthony Coman <acoman@yahoo.com>
2021-12-15 14:12:49 -0600

    MVP

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import MovieList from './components/MovieList';
 import Movie from './components/Movie';
 
 import MovieHeader from './components/MovieHeader';
+import EditMovieForm from './components/EditMovieForm';
+import AddMovieForm from './components/AddMovieForm';
 
 import FavoriteMovieList from './components/FavoriteMovieList';
 
@@ -15,7 +17,7 @@ const App = (props) => {
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
   useEffect(()=>{
-    axios.get('http://localhost:5001/api/movies')
+    axios.get('http://localhost:9000/api/movies')
       .then(res => {
         setMovies(res.data);
       })
@@ -25,6 +27,7 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+      setMovies(movies.filter(movie=> (movie.id !== id)));
   }
 
   const addToFavorites = (movie) => {
@@ -43,11 +46,16 @@ const App = (props) => {
           <FavoriteMovieList favoriteMovies={favoriteMovies}/>
         
           <Switch>
-            <Route path="/movies/edit/:id">
+            <Route path='/movies/add'>
+              <AddMovieForm setMovies={setMovies}/>
+            </Route>
+
+            <Route path="/movies/edit/:id" >
+              <EditMovieForm setMovies={setMovies}/>
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie}/>
             </Route>
 
             <Route path="/movies">
